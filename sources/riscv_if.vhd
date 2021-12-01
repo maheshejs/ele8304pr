97d62de10ce7a16b21c8ef8f6d0a1e199bea664c
Joseph Maheshe <maheshejs@gmail.com>
2021-11-30 23:47:03 -0500

    Lab2 :  Refactored Instruction Fetch code

--- sources/riscv_if.vhd
+++ sources/riscv_if.vhd
@@ -19,10 +19,7 @@ entity riscv_if is
       i_clk       : in std_logic;
       i_rstn      : in std_logic;
       --
-      i_flush     : in std_logic;
-      i_stall     : in std_logic;
-      i_transfert : in std_logic;
-      i_target    : in std_logic_vector(XLEN-1 downto 0);
+      i_ex        : in E_EX;
       --
       i_imem_read : in std_logic_vector(DPM_WIDTH-1 downto 0);
       --
@@ -44,20 +41,20 @@ begin
   port map (
     i_clk         => i_clk,
     i_rstn        => i_rstn,
-    i_stall       => i_stall,
-    i_transfert   => i_transfert,
-    i_target      => i_target,
+    i_stall       => i_ex.stall,
+    i_transfert   => i_ex.transfert,
+    i_target      => i_ex.target,
     o_pc          => s_pc
   );
 
   P_REG_IF_ID : process(i_clk, i_rstn)
   begin
     if (rising_edge(i_clk)) then
-      if (i_stall = '0') then
+      if (i_ex.stall = '0') then
         s_reg_if_id.imem_read <= i_imem_read; 
       end if;
 
-      if (i_flush = '1') then
+      if (i_ex.flush = '1') then
         s_reg_if_id.imem_read <= (others => '0'); 
       end if;
     end if;
