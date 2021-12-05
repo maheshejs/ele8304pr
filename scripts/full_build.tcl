49aa68a816ecde59d0b9153497592f0bcc0b8536
8304_12 <8304_12@vlsi410.vlsi.polymtl.ca>
2021-12-05 14:53:29 -0500

    Lab2: New synthetis results with clock gating and updated timing constraints

--- scripts/full_build.tcl
+++ scripts/full_build.tcl
@@ -6,6 +6,7 @@ read_libs -max_libs slow_vdd1v0_basicCells.lib -min_libs fast_vdd1v0_basicCells.
 read_physical -lef gsclib045_tech.lef
 read_qrc gpdk045.tch
 set_db interconnect_mode ple
+set_db lp_insert_clock_gating true
 read_hdl -vhdl { riscv_pkg.vhd riscv_half_adder.vhd riscv_adder.vhd riscv_logic.vhd riscv_alu.vhd riscv_pc.vhd riscv_rf.vhd riscv_if.vhd riscv_id.vhd riscv_ex.vhd riscv_me.vhd riscv_wb.vhd riscv_core.vhd }
 elaborate riscv_core
 check_design -unresolved
@@ -17,7 +18,6 @@ set_db syn_generic_effort high
 syn_generic riscv_core
 write_hdl > $::env(SYN_NET_DIR)/riscv_core.syn_gen.v
 set_db syn_map_effort high
-syn_map compteur
 syn_map riscv_core
 write_hdl > $::env(SYN_NET_DIR)/riscv_core.syn_map.v
 set_db syn_opt_effort high
@@ -27,7 +27,6 @@ write_hdl > $::env(SYN_NET_DIR)/riscv_core.syn.v
 write_sdf -nonegchecks -setuphold split -version 2.1 > $::env(SYN_NET_DIR)/riscv_core.syn.sdf
 write_sdc > $::env(CONST_DIR)/riscv_core.syn.sdc
 report_timing > $::env(SYN_REP_DIR)/riscv_core.syn.timing.rpt
-report_timing -lint > $::env(SYN_REP_DIR)/riscv_core.syn.timing.rpt
 report_area > $::env(SYN_REP_DIR)/riscv_core.syn.area.rpt
 report_gates > $::env(SYN_REP_DIR)/riscv_core.syn.gates.rpt
 report_power > $::env(SYN_REP_DIR)/riscv_core.syn.power.rpt
